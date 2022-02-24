e98eac06b23b4d04453f09c97ef8344a84a1f62b
andiswa-sinxo <andiswasinxo@gmail.com>
2022-02-24 12:04:43 +0200

    error message

--- index.js
+++ index.js
@@ -3,25 +3,35 @@ let randomNumber = Math.ceil((Math.random() * 100))
 const guess_number = document.querySelector(".guess_number")
 const btn = document.querySelector(".btn")
 const message = document.querySelector(".message")
+const errorMsg = document.querySelector(".errorMsg")
 
 
 function guessBtnClicked(){
     const guessedNo = Number(guess_number.value);
 
-    if (guessedNo < randomNumber){ 
-        message.innerHTML = "Your guess is too low";
-
+    if(guessedNo <= 1 || guessedNo >= 100){
+        errorMsg.innerHTML = "Please choose a number between 1 and 100"
+        errorMsg.classList.add("errorColour");
         setTimeout(function(){
-            message.innerHTML = "";
+            errorMsg.innerHTML = ""
+        }, 2000);
+    }else if (guessedNo < randomNumber){ 
+        errorMsg.innerHTML = "Your guess is too low";
+        errorMsg.classList.add("errorColour");
+        setTimeout(function(){
+            errorMsg.innerHTML = "";
         }, 2000);
 
     }else if (guessedNo > randomNumber){
-        message.innerHTML = "Your guess is too high";
+        errorMsg.innerHTML = "Your guess is too high";
+        errorMsg.classList.add("errorColour");
         setTimeout(function(){
-            message.innerHTML = "";
+            errorMsg.innerHTML = "";
         }, 2000);
-    }else {
+
+     }else{
           message.innerHTML = `Correct, the secret number is ${guessedNo} !!!`;
+          message.classList.add("message")
           randomNumber = Math.ceil((Math.random() * 100))
           setTimeout(function(){
             message.innerHTML = "";
@@ -29,11 +39,16 @@ function guessBtnClicked(){
 
           setTimeout(function(){
             message.innerHTML = "New game has started!!";
+            message.classList.add("message")
+
         }, 3000);
-        
-    } 
+    }
 
 }
+
+// function refresh(){
+//     location.reload()
+// }
 
 
 btn.addEventListener('click', guessBtnClicked)
